9992ace251f4631c28e25aa947c31104edc6b687
jonsamp <sampjon@gmail.com>
2020-09-17 09:26:11 -0400

    Updates title

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       }`}
     >
       <Head>
-        <title>Jon Samp's Website</title>
+        <title>Jon Samp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -103,17 +103,6 @@ export default function Home() {
           ))}
         </section>
       </main>
-
-      <footer className={styles.footer}>
-        {/* <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{" "}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a> */}
-      </footer>
     </div>
   )
 }
